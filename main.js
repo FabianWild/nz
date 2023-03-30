c6ca42217a2d0303659ac114015dbac58177887a
FabianWild <Fabian.Wild1996@gmx.de>
2023-03-30 16:08:04 +0200

    STOP -->  STOPS

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let stop_lat = -40.833333;
 let stop_lon = 172.9;
 let zoom = 13;
-let title = "Abel Tasman Nationalpark"
+let title = "Abel Tasman Nationalpark";
+
+const STOPS = ["Franz-Josef Gletscher", "Abel Tasman Nationalpark", "Picton"];
+console.log(STOPS)
+console.log(STOPS[0]);
+console.log(STOPS[1]);
+console.log(STOPS[2]);
+console.log(STOPS.length);
+
+
 let map = L.map('map').setView([stop_lat, stop_lon], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
