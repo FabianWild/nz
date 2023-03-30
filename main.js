b5d129a5874cccffeab556853d386eacd2b1c379
FabianWild <Fabian.Wild1996@gmx.de>
2023-03-30 15:52:57 +0200

    nochmal

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var map = L.map('map').setView([-40.833333, 172.9], 13);
+let stop_lat = -40.833333;
+let stop_lon = 172.9;
+let zoom = 13;
+let title = "Abel Tasman Nationalpark"
+let map = L.map('map').setView([stop_lat, stop_lon], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-40.833333, 172.9]).addTo(map)
-    .bindPopup('Abel Tasman Nationalpark')
+L.marker([stop_lat, stop_lon]).addTo(map)
+    .bindPopup(title)
     .openPopup(); 
